9e3eea1e1b4a79d0fc6b5d9676184a48c9ace97c
genki.sano <g.sano.2441@gmail.com>
2023-05-12 22:14:35 +0900

    feat: ベーシック認証を追加する

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_BASE_URL: string
+      NEXT_PUBLIC_BASIC_REALM_USER: string
+      NEXT_PUBLIC_BASIC_REALM_PWD: string
       NEXT_PUBLIC_GOOGLE_ANALYTICS_ID?: string
       NEXT_PUBLIC_GOOGLE_ADSENSE_ID?: string
     }
