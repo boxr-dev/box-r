5ae574c79ac1f370c751397a01835f0da604645b
genki.sano <g.sano.2441@gmail.com>
2023-05-27 18:15:23 +0900

    perf: svgファイルをインラインコードで埋め込む

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import { MdMenu } from 'react-icons/md'
 import style from '@/components/Layout/Header/Header.module.scss'
 import { NavItem } from '@/components/Layout/Header/NavItem'
+import TitleImage from 'public/images/logo/title.svg'
 
 export const Header: React.FC = () => {
   return (
@@ -13,7 +13,7 @@ export const Header: React.FC = () => {
         </div>
         <h1 className={style.logo}>
           <Link href={'/'}>
-            <Image src={'/images/logo/title.svg'} alt='BOX R' width={150} height={64} />
+            <TitleImage role='img' aria-label='BOX R' />
           </Link>
         </h1>
         <nav className={style.navi}>
