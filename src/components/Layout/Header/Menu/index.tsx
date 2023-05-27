e6a6f0a8526d4206489ffc6d12a04bad5d433333
genki.sano <g.sano.2441@gmail.com>
2023-05-27 22:25:38 +0900

    style: ナビゲーションドロワーにスタイルを適用する

--- src/components/Layout/Header/Menu/index.tsx
+++ src/components/Layout/Header/Menu/index.tsx
@@ -1,28 +1,24 @@
+import Image from 'next/image'
+import { FaInstagram, FaCaretRight } from 'react-icons/fa'
+import { IconLink } from '@/components/Common/IconLink'
 import { Link } from '@/components/Common/Link'
+import style from '@/components/Layout/Header/Menu/Menu.module.scss'
 
 export const Menu: React.FC = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
+      <div className={style.logo}>
+        <Image src={'/images/logo/main.png'} alt='BOX R' width={150} height={150} />
+      </div>
+      <nav className={style.map}>
+        <ul className={style['map-list']}>
+          <li className={style['map-list-item']}>
             <Link href={'/about'}>BOX Rについて</Link>
           </li>
-          <li>
+          <li className={style['map-list-item']}>
             <Link href={'/lesson'}>レッスン</Link>
-            <ul>
-              <li>
-                <Link href={'/lesson/chabako'}>インテリア茶箱</Link>
-              </li>
-              <li>
-                <Link href={'/lesson/cartonnage'}>カルトナージュ</Link>
-              </li>
-              <li>
-                <Link href={'/lesson/french-deco'}>フレンチ メゾン デコール</Link>
-              </li>
-            </ul>
           </li>
-          <li>
+          <li className={style['map-list-item']}>
             <Link
               href={
                 'https://calendar.google.com/calendar/u/0/embed?src=a7dctp6ugi964437og5b02hdds@group.calendar.google.com&ctz=Asia/Tokyo'
@@ -32,21 +28,31 @@ export const Menu: React.FC = () => {
               スケジュール
             </Link>
           </li>
-          <li>
+          <li className={style['map-list-item']}>
             <Link href={'https://ameblo.jp/boxr-chabako'} isExternalLink>
               ブログ
             </Link>
           </li>
-          <li>
+          <li className={style['map-list-item']}>
             <Link href={'https://ssl.form-mailer.jp/fms/2737b864408251'} isExternalLink>
               お問い合わせ
             </Link>
           </li>
-          <li>
-            <Link href={'/privacy'}>プライバシーポリシー</Link>
-          </li>
         </ul>
       </nav>
+      <div className={style.sns}>
+        <div className={style['sns-list']}>
+          <span className={style['sns-list-text']}>Follow on</span>
+          <span className={style.caret}>
+            <FaCaretRight />
+          </span>
+          <div className={style['instagram-icon']}>
+            <IconLink href={'https://www.instagram.com/boxr_rieko'} isExternalLink>
+              <FaInstagram />
+            </IconLink>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
