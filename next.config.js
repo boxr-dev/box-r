2b3feb03e98d1660a670ce865b259a07b063598f
genki.sano <g.sano.2441@gmail.com>
2023-05-27 18:08:51 +0900

    chore: svgファイルをインラインコードで埋め込めるようにする

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.tsx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: true, // 画像が欠けちゃうようになったら無効にする
+          },
+        },
+      ],
+    })
+    return config
+  },
+  images: {
+    disableStaticImages: true, // 自前のSVGの型定義を有効化するため
+  },
 }
 
 module.exports = nextConfig
