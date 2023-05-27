ce28e69630de359bf7776f1b83da228a1f7e836e
genki.sano <g.sano.2441@gmail.com>
2023-05-27 18:50:26 +0900

    style: 404ページをモバイル対応する

--- src/components/404/index.tsx
+++ src/components/404/index.tsx
@@ -3,16 +3,18 @@ import { BottonLink } from '@/components/Common/BottonLink'
 
 export const NotFound: React.FC = () => {
   return (
-    <div className={style.container}>
+    <main className={style.container}>
       <h1 className={style.main}>
         <div className={style['heading-eng']}>404</div>
         <div className={style['not-found']}>Page not found</div>
       </h1>
-      <p>お探しのページは見つかりませんでした。</p>
-      <p>こちらのページはすでに削除されたか、もしくはURLが異なる場合があります。</p>
+      <div className={style.deiscription}>
+        <p>お探しのページは見つかりませんでした。</p>
+        <p>こちらのページはすでに削除されたか、もしくはURLが異なる場合があります。</p>
+      </div>
       <div className={style.btn}>
         <BottonLink href={'/'}>TOPへ戻る</BottonLink>
       </div>
-    </div>
+    </main>
   )
 }
