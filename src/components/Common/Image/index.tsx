c0993827b0104d60d23098fb7bc62e11d646a899
genki.sano <g.sano.2441@gmail.com>
2023-05-28 21:45:36 +0900

    perf: next/imageをラップしたコンポーネントを実装する
    既存のImageコンポーネントをそのまま使った場合
    読み込み完了次第、画像がパッと表示されるので少し不格好になってしまう。
    
    そのため、画像の読み込み完了後にフェードインして表示させるようにする。

---
+++ src/components/Common/Image/index.tsx
@@ -0,0 +1,17 @@
+import NextImage, { ImageProps } from 'next/image'
+import { ReactEventHandler } from 'react'
+import style from '@/components/Common/Image/Image.module.scss'
+
+export const Image: React.FC<ImageProps> = (props) => {
+  const onLoad: ReactEventHandler<HTMLImageElement> = (e) => {
+    if (e.currentTarget.srcset) {
+      e.currentTarget.dataset.load = 'done'
+    }
+  }
+
+  return (
+    <span className={style.container}>
+      <NextImage {...props} onLoad={onLoad} />
+    </span>
+  )
+}
