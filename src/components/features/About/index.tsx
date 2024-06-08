26b1e21eddd3057ae24020d25fb051cd42acff6e
genki.sano <g.sano.2441@gmail.com>
2024-06-08 17:27:42 +0900

    refactor: aboutページの処理をコンポーネントに分割する

---
+++ src/components/features/About/index.tsx
@@ -0,0 +1,9 @@
+import style from '@/components/features/About/About.module.scss'
+
+export const About: React.FC = () => {
+  return (
+    <div className={style.container}>
+      <span>About</span>
+    </div>
+  )
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import { Layout } from '@/components/commons/Layout'
+import { About } from '@/components/features/About'
 
 const AboutPage: NextPage = () => {
   return (
     <Layout title={'BOX R について | BOX R'} description={'ディスクリプション'} siteName={'BOX R'}>
-      <span>About</span>
+      <About />
     </Layout>
   )
 }
