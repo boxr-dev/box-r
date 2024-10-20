add5cff40725be2549e28f5cf0cc58cfbbbf06f2
genki.sano <g.sano.2441@gmail.com>
2024-10-21 06:36:09 +0900

    update: 本番環境で公開する

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest) {
-  // ベーシック認証は本番環境だけで適用させたい
-  if (process.env.NODE_ENV !== 'production') {
-    return NextResponse.next()
-  }
-
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
-
-    if (user === process.env.NEXT_PUBLIC_BASIC_REALM_USER && pwd === process.env.NEXT_PUBLIC_BASIC_REALM_PWD) {
-      return NextResponse.next()
-    }
-  }
-  url.pathname = '/api/auth'
-
-  return NextResponse.rewrite(url)
+export function middleware() {
+  // ベーシック認証をつけたいときに使う
+  return NextResponse.next()
 }
