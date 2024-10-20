6dfd9b671f5a32bb8e63dfbc495b042c6d5e600a
genki.sano <g.sano.2441@gmail.com>
2024-10-21 06:38:23 +0900

    chore: 環境変数を整理

--- environment.d.ts
+++ environment.d.ts
@@ -2,10 +2,9 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_BASE_URL: string
-      NEXT_PUBLIC_BASIC_REALM_USER: string
-      NEXT_PUBLIC_BASIC_REALM_PWD: string
-      NEXT_PUBLIC_GOOGLE_ANALYTICS_ID?: string
-      NEXT_PUBLIC_GOOGLE_ADSENSE_ID?: string
+      NEXT_PUBLIC_GA_ID: string
+      NEXT_PUBLIC_BASIC_REALM_USER?: string
+      NEXT_PUBLIC_BASIC_REALM_PWD?: string
     }
   }
 }
