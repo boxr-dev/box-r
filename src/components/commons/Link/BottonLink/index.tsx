f298ebae3fdc61883a97cf6f5ebd99e6e2fae976
genki-sano <g.sano.2441@gmail.com>
2025-06-22 18:40:44 +0900

    style: outlinedオプションを追加

--- src/components/commons/Link/BottonLink/index.tsx
+++ src/components/commons/Link/BottonLink/index.tsx
@@ -4,12 +4,17 @@ import style from '@/components/commons/Link/BottonLink/BottonLink.module.scss'
 
 type Props = PropsWithChildren<{
   href: string
+  variant?: 'contained' | 'outlined'
   isExternalLink?: boolean
 }>
 
-export const BottonLink: React.FC<Props> = ({ children, href, isExternalLink }) => {
+export const BottonLink: React.FC<Props> = ({ children, href, variant, isExternalLink }) => {
   return (
-    <button className={style.container} type='button' tabIndex={-1}>
+    <button
+      className={`${style.container} ${variant === 'outlined' ? style.outlined : style.contained}`}
+      type='button'
+      tabIndex={-1}
+    >
       <Link className={style.link} href={href} isExternalLink={isExternalLink || false}>
         {children}
       </Link>
