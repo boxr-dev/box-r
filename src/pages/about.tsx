17a8f26c7dff47c6c41fff23a339c68e6611d672
genki-sano <g.sano.2441@gmail.com>
2025-06-22 22:45:25 +0900

    chore: SEO対策で構造化データを追加

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,36 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import { Layout } from '@/components/commons/Layout'
 import { About } from '@/components/features/About'
 
 const AboutPage: NextPage = () => {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: '佐野里江子',
+    alternateName: 'Rieko Sano',
+    jobTitle: 'カルトナージュ・インテリア茶箱アーティスト',
+    url: 'https://boxr.jp/about',
+    image: 'https://boxr.jp/images/about/profile.jpg',
+    sameAs: ['https://www.instagram.com/boxr_rieko/'],
+    description:
+      '佐野里江子は、神奈川県横浜市在住のカルトナージュ・インテリア茶箱アーティスト。BOX Rを主宰し、オリジナル作品の制作やレッスンを行っている。',
+  }
+
   return (
-    <Layout
-      title={'BOX R について | BOX R'}
-      description={
-        '佐野里江子 | BOX R は、カルトナージュとインテリア茶箱のアトリエです。インテリアに馴染みつつも存在感のある作品作りを心がけております。'
-      }
-    >
-      <About />
-    </Layout>
+    <>
+      <Head>
+        <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      </Head>
+      <Layout
+        title={'BOX R について | 佐野里江子'}
+        description={
+          'BOX R は、カルトナージュとインテリア茶箱のアトリエです。インテリアに馴染みつつも存在感のある作品作りを心がけております。'
+        }
+      >
+        <About />
+      </Layout>
+    </>
   )
 }
 
